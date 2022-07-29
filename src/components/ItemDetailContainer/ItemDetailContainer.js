4e30c3f02bd9a6cc49754d46782f9ed874463c01
Fernando D'abenigno <dabenignofernando32@gmail.com>
2022-07-28 22:38:42 -0300

    firebase1

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,44 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { getFirestore, doc , getDoc } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
-import { getProd } from '../../Mocks/FakeApi';
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState({});
     const [loading, setLoading] = useState(true);
 
-    const { id, detalleId } = useParams();
-
+    const { id } = useParams();
 
     useEffect(() => {
         setLoading(true);
-        const URL = `https://fakestoreapi.com/products/${id}`;
-        fetch(URL)
-            .then((res) => res.json())
-            .then((json) => setProduct(json))
-            .finally(() => {
-                setLoading(false);
-            });
-
-        getProd(id)
-            .then((res) => {
-                setProduct(res);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-            .finally(() => {
-                setLoading(false);
-            }); 
-    }, [id]);
-
-    useEffect(() => {
         const querydb = getFirestore();
-        const queryDoc = doc(querydb, 'productos', 'detalleId');
+        const queryDoc = doc(querydb, 'productos', id);
         getDoc(queryDoc)
-        .then(res => setProduct({id: res.id, ...res.data}));
-     }, [detalleId]);
+        .then(res => setProduct({id: res.id, ...res.data() }))
+        .then(setLoading(false));
+     }, [id]);
 
     //console.log(product);
     return (
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getFirestore, collection , getDocs, query, where } from 'firebase/firestore';
 import ItemList from '../ItemList';
-import { getProds } from '../../Mocks/FakeApi';
 import { useParams } from 'react-router-dom';
 
 const ItemListContainer = () => {
@@ -13,41 +12,17 @@ const ItemListContainer = () => {
    
     useEffect(() => {
         setLoading(true);
-
-        const URL = categoryId 
-            ? `https://fakestoreapi.com/products/category/${categoryId}`
-            : 'https://fakestoreapi.com/products';
-
-
-        fetch(URL)
-            .then((res) => res.json())
-            .then((json) => setProducts(json))
-            .finally(() => {
-                setLoading(false);
-            });
-
-         getProds(categoryId)
-            .then((res) => {
-                setProducts(res);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-            .finally(() => {
-                setLoading(false);
-            }); 
-    }, [categoryId]);
-
-    useEffect(() => {
         const querydb = getFirestore();
         const queryCollection = collection(querydb, 'productos');
         if(categoryId){
         const queryFilter = query(queryCollection, where('category', '==', categoryId))
         getDocs(queryFilter)    
-            .then(res => setProducts (res.docs.map(product => ({id: product.id, ...product.data()}))));    
+            .then(res => setProducts (res.docs.map(product => ({id: product.id, ...product.data()}))));
+              
         } else{
             getDocs(queryCollection)
-                .then(res => setProducts (res.docs.map(product => ({id: product.id, ...product.data()}))));   
+                .then(res => setProducts (res.docs.map(product => ({id: product.id, ...product.data()}))))
+                .then(setLoading(false));   
         }
      }, [categoryId]);
 
